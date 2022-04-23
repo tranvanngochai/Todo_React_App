0fc92183373f0b9d72e9698b95db9c52b9186525
DESKTOP-U8BQSFJ\Bin Bin...nef <tranvanngochai13@gmail.com>
2022-04-24 00:48:48 +0700

    update run build

---
+++ build/precache-manifest.0408e780429ddc285d1b14176822c2c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3948834e9f2a6d1c770e09b814137042",
+    "url": "/index.html"
+  },
+  {
+    "revision": "548dadfd3dfa798dddbb",
+    "url": "/static/css/main.4e987138.chunk.css"
+  },
+  {
+    "revision": "8e49ff261b0b40148c6b",
+    "url": "/static/js/2.c6b53d6d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.c6b53d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "548dadfd3dfa798dddbb",
+    "url": "/static/js/main.5277e2c3.chunk.js"
+  },
+  {
+    "revision": "ed72fd0300eba1b1f74d",
+    "url": "/static/js/runtime-main.258720a7.js"
+  }
+]);
